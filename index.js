311d3acbfde6e1ff964d3d7a67f4be7451f735a6
Escael01 <j.aquebeque@gmail.com>
2025-11-28 17:25:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Contenedor está corriendo 🚀");
+function suma(a, b) {
+  return a + b;
+}
+
+function resta(a, b) {
+  return a - b;
+}
+
+console.log("Resultado suma:", suma(2, 3));
+console.log("Resultado resta:", resta(5, 2));
