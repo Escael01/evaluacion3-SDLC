ccc1402ca25889f2f37e2486a01d690c8bc63035
Escael01 <j.aquebeque@gmail.com>
2025-11-28 18:33:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function resta(a, b) {
 
 console.log("Resultado suma:", suma(2, 3));
 console.log("Resultado resta:", resta(5, 2));
+
+// Exportar funciones para que Jest las use
+module.exports = { suma, resta };
