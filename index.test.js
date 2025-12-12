a1e40764422cfeec11f06d5a2d7e05bace6f2ed0
Escael01 <j.aquebeque@gmail.com>
2025-12-12 16:31:25 -0300

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,10 +1,9 @@
-const suma = (a, b) => a + b;
-const resta = (a, b) => a - b;
+const { sumar, restar } = require('./src/calculadora');
 
 test('suma 2 + 3 = 5', () => {
-  expect(suma(2, 3)).toBe(5);
+  expect(sumar(2, 3)).toBe(5);
 });
 
 test('resta 5 - 2 = 3', () => {
-  expect(resta(5, 2)).toBe(3);
+  expect(restar(5, 2)).toBe(3);
 });
